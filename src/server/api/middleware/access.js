b650eae2347164a28430e49091d18f9251303a21
zhengjunxiang <junxiang.zhengbitmain.com>
2018-05-29 22:26:55 +0800

    fix signup

--- src/server/api/middleware/access.js
+++ src/server/api/middleware/access.js
@@ -1,7 +1,10 @@
 module.exports = function(req, res, next) {
   const path = req.path;
-  if (path === '/api/user/delete.json' || path === '/api/user/signup.json') {
+  if (path === '/api/user/delete.json') {
     if (req.session.user.access !== 0) return res.json({ errno: 1, mes: '没有权限' })
     next()
+  } else if (path === '/api/user/signup.json') {
+    if (req.body.access === 0) return next()
+    else if (req.session.user.access !== 0) return res.json({ errno: 1, mes: '没有权限' })
   } else next()
 }
